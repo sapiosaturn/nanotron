fd2e00f7a9ae63be0c40c1b0c918341815ca50b0
Varun Singh <varun@arcee.ai>
2025-02-01 18:42:14 -0700

    testing adding new model type

--- src/nanotron/trainer.py
+++ src/nanotron/trainer.py
@@ -59,6 +59,7 @@ from nanotron.models import NanotronModel, build_model
 from nanotron.models.base import check_model_has_grad
 from nanotron.models.llama import LlamaForTraining, RotaryEmbedding
 from nanotron.models.starcoder2 import Starcoder2ForTraining
+from nanotron.models.vellichor import VellichorForTraining
 from nanotron.optim.clip_grads import clip_grad_norm
 from nanotron.parallel import ParallelContext
 from nanotron.parallel.data_parallel.utils import sync_gradients_across_dp
@@ -105,6 +106,7 @@ dist_logger.setLevel(logging.WARNING)
 CONFIG_TO_MODEL_CLASS = {
     "LlamaConfig": LlamaForTraining,
     "Starcoder2Config": Starcoder2ForTraining,
+    "VellichorConfig": VellichorForTraining,
 }
 
 try:
